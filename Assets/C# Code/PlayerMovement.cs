c01193afe2ce79e2c0d2f1f0b902f95bedc9cf9f
Guidoge3 <70884054+Guidoge3@users.noreply.github.com>
2021-11-11 16:11:41 +0100

    bonk

---
+++ Assets/C# Code/PlayerMovement.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float playerSpeed = 5.0f;
+    public float jumpPower = 5.0f;
+    public float gravity = -9.81f;
+
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    private Rigidbody2D _playerRigidbody;
+    private bool _facingRight;
+
+    bool isGrounded;
+    Vector2 velocity;
+
+
+
+    private void Start()
+    {
+        _playerRigidbody = GetComponent<Rigidbody2D>();
+        _facingRight = true;
+    }
+    private void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        MovePlayer();
+        if (Input.GetKeyDown(KeyCode.D) && !_facingRight)
+        {
+            flip();
+            _facingRight = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A) && _facingRight)
+        {
+            flip();
+            _facingRight = false;
+        }
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpPower * -2f * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+    }
+    private void MovePlayer()
+    {
+        var horizontalInput = Input.GetAxisRaw("Horizontal");
+        _playerRigidbody.velocity = new Vector2(horizontalInput * playerSpeed, _playerRigidbody.velocity.y);
+    }
+
+    private void flip()
+    {
+        transform.Rotate(0f, 180, 0f);
+    }
+}
---
+++ Assets/C# Code/Weapon.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class Weapon : MonoBehaviour
+{
+    //private Animator anim;
+
+
+    public Transform firePoint;
+    public GameObject bulletPrefab;
+
+    private float reloadTime = 0.4f;
+
+    void Start()
+    {
+        //anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1") && reloadTime <= 0)
+        {
+            Shoot();
+            reloadTime = 0.25f;
+        }
+        reloadTime -= Time.deltaTime;
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        //anim.SetBool("Fire", true);
+    }
+}
